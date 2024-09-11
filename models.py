0262c4af6efc0f1d7f00eb478225336bfca23e5c
karma <karmatenpa9845@gmail.com>
2024-09-11 17:32:30 -0500

    Adding gitignore, requirements and additional imports. Also connecting db to flask app.

--- models.py
+++ models.py
@@ -1,1 +1,16 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.model):
+    """User for application"""
+
+
+
+def connect_db(app):
+    """Connecting database to flask app"""
+
+    db.app = app
+    db.init_app(app)
